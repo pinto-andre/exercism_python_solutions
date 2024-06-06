56ce114f0e1bf42ae9a166aa23a7da4867b6b918
Andre Pinto <andre.marpinto@gmail.com>
2024-06-06 18:36:32 +0100

    rotational cipher exercise done with type hinting

---
+++ python/rotational-cipher/rotational_cipher.py
@@ -0,0 +1,28 @@
+"""Function to create a rotation cypher: aka Caesar cipher"""
+from string import ascii_lowercase, ascii_uppercase
+
+def rotate(text: str, key: int) -> str:
+    """ Function to create a rotation cypher: aka Caesar cipher.
+    
+    :param text: str - Given string to turn to cipher
+    :param key: int - key deciding how many rotations the letter will have
+    :returns: str - cipher
+    :raises: TypeError - Only string text and integer keys are allowed.
+    :raises: ValueError - Key value must be between 0 and 26."""
+
+    if not isinstance(text, str) or not isinstance(key, int):
+        raise TypeError("Only string text and integer keys are allowed.")
+    if not 0 <= key <= 26:
+        raise ValueError("Key value must be between 0 and 26.")
+
+    rotated = ""
+
+    for char in text:
+        if char.islower():
+            rotated += ascii_lowercase[(ascii_lowercase.index(char) + key) % 26]
+        if char.isupper():
+            rotated += ascii_uppercase[(ascii_uppercase.index(char) + key) % 26]
+        else:
+            rotated += char
+
+    return rotated
