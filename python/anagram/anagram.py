6bf67314b6de1dccba634f67d85e8cfbfc6f8c3a
Andre Pinto <andre.marpinto@gmail.com>
2024-06-23 00:06:54 +0100

    added type hints to anagrams

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,4 +1,19 @@
-def find_anagrams(word, candidates):
+"""Function to find anagrams"""
+from typing import List
+def find_anagrams(word: str, candidates: List[str]) -> List[str]:
+    """Function to find anagrams
+    :param word: str - given word to find anagrams
+    :param candidates: List[str] - List of possible anagrams
+    :raises: TypeError - word must be a string
+    :raises: TypeError - Only lists of strings are accepted
+    :returns: List[str] - list with anagrams
+"""
+    if not isinstance(word, str):
+        raise TypeError('word must be a string')
+
+    if not all(isinstance(item, str) for item in candidates):
+        raise TypeError('All items in the list must be strings')
+
     anagrams = []
     for item in candidates:
         if str(''.join(sorted(item.lower()))) == str(''.join(sorted(word.lower()))) and item.lower() != word.lower():
