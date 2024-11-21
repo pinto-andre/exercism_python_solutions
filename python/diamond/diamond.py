e1266a9108eeaabee4ecb17a5281ee4f70f815cf
Andre Pinto <andre.marpinto@gmail.com>
2024-11-21 19:27:36 +0000

    Added diamond version with comprehension usage

--- python/diamond/diamond.py
+++ python/diamond/diamond.py
@@ -27,3 +27,26 @@ def rows(letter: str) -> List:
     diamond.extend(reversed(diamond[:-1]))
 
     return diamond
+
+# Version with comprehension
+# def rows(letter: str) -> List:
+#     """
+#     Function to create a diamond shape according to the given alphabet letter
+#     """
+#     if not isinstance(letter, str):
+#         raise TypeError("Input must be a string")
+#     if len(letter) > 1 or letter not in ALPHABET:
+#         raise ValueError("Input must be a single and uppercased letter.")
+
+#     max_index = ALPHABET.index(letter)  # Index of the letter in the alphabet
+
+#     # Build the top half (including the middle row) using a comprehension
+#     diamond = [
+#         f"{' ' * (max_index - i)}{ALPHABET[i]}{' ' * (2 * i - 1)}{ALPHABET[i] if i > 0 else ''}{' ' * (max_index - i)}"
+#         for i in range(max_index + 1)
+#     ]
+
+#     # Mirror the top half (excluding the middle row) to complete the diamond
+#     diamond.extend(reversed(diamond[:-1]))
+
+#     return diamond
